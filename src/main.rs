d3ae8e63defb776860b46148b93eb4b688ff9e4f
Neil Williams <neil@spladug.net>
2023-12-01 16:31:14 -0800

    Day 1, part 2
    Holy hell I'm not proud of this one. Gross!

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,56 @@
 use std::fs::File;
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader};
 
 fn parse_digits(calibration: &str) -> u32 {
-    let first_digit = calibration
-        .chars()
-        .filter(char::is_ascii_digit)
-        .next()
-        .unwrap()
-        .to_digit(10)
-        .unwrap();
+    let patterns = HashMap::from([
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
 
-    let second_digit = calibration
-        .chars()
-        .rev()
-        .filter(char::is_ascii_digit)
-        .next()
-        .unwrap()
-        .to_digit(10)
-        .unwrap();
+    let mut first_digit_index = calibration.len();
+    let mut first_digit = 0;
+    for (pattern, digit) in patterns.iter() {
+        if let Some(index) = calibration.find(pattern) {
+            if index < first_digit_index {
+                first_digit_index = index;
+                first_digit = *digit;
+            }
+        }
+    }
+    assert_ne!(first_digit, 0);
+
+    let mut second_digit_index: i64 = -1;
+    let mut second_digit = 0;
+    for (pattern, digit) in patterns.iter() {
+        if let Some(index) = calibration.rfind(pattern) {
+            if index as i64 > second_digit_index {
+                second_digit_index = index as i64;
+                second_digit = *digit;
+            }
+        }
+    }
+
+    if second_digit == 0 {
+        println!("oh noes {calibration}")
+    }
+    assert_ne!(second_digit, 0);
 
     first_digit * 10 + second_digit
 }
@@ -42,9 +75,14 @@ mod tests {
 
     #[test]
     fn check_digit_parsing() {
-        assert_eq!(parse_digits("1abc2"), 12);
-        assert_eq!(parse_digits("pqr3stu8vwx"), 38);
-        assert_eq!(parse_digits("a1b2c3d4e5f"), 15);
-        assert_eq!(parse_digits("treb7uchet"), 77);
+        assert_eq!(parse_digits("two1nine"), 29);
+        assert_eq!(parse_digits("eightwothree"), 83);
+        assert_eq!(parse_digits("abcone2threexyz"), 13);
+        assert_eq!(parse_digits("xtwone3four"), 24);
+        assert_eq!(parse_digits("4nineeightseven2"), 42);
+        assert_eq!(parse_digits("zoneight234"), 14);
+        assert_eq!(parse_digits("7pqrstsixteen"), 76);
+        assert_eq!(parse_digits("twone"), 21);
+        assert_eq!(parse_digits("7qpnldcvgs"), 77);
     }
 }
