40892ce1f690534d7455067c73d18250892c9f6a
Neil Williams <neil@spladug.net>
2023-12-09 11:29:53 -0800

    Day 5, part 1

---
+++ day5/src/main.rs
@@ -0,0 +1,172 @@
+use std::str::FromStr;
+use std::vec::Vec;
+use std::fs::read_to_string;
+use std::error::Error;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct Range {
+    source_start: u32,
+    length: u32,
+    dest_start: u32,
+}
+
+impl FromStr for Range {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let values: Vec<u32> = s.split_whitespace()
+            .map(|n| 
+                n.parse::<u32>()
+                .map_err(|err| Box::new(err) as Box<dyn Error>)
+            )
+            .collect::<Result<Vec<u32>, Box<dyn Error>>>()?;
+
+        if let [dest_start, source_start, length] = values[..] {
+            Ok(Range {
+                source_start,
+                length,
+                dest_start,
+            })
+        } else {
+            Err("wrong number of numbers on range line".into())
+        }
+    }
+}
+
+struct Map {
+    ranges: Vec<Range>,
+}
+
+impl FromStr for Map {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut ranges: Vec<Range> = s
+            .lines()
+            .skip(1)
+            .map(|line| line.parse::<Range>())
+            .collect::<Result<Vec<Range>, Box<dyn Error>>>()?;
+        ranges.sort();
+
+        Ok(Map { ranges })
+    }
+}
+
+impl Map {
+    fn translate(&self, input: u32) -> u32 {
+        for range in self.ranges.iter() {
+            if range.source_start > input {
+                break;
+            }
+
+            let offset = input - range.source_start;
+            if offset < range.length {
+                return range.dest_start + offset;
+            }
+        }
+
+        input
+    }
+}
+
+struct Atlas {
+    seeds: Vec<u32>,
+    maps: Vec<Map>,
+}
+
+impl FromStr for Atlas {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (seeds_raw, rest_raw) = s
+            .split_once("\n\n")
+            .ok_or("couldn't split seeds line")?;
+
+        let seeds: Vec<u32> = seeds_raw
+            .strip_prefix("seeds: ")
+            .ok_or("didn't have seeds prefix")?
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+
+        let maps: Vec<Map> = rest_raw
+            .split("\n\n")
+            .map(|graf| graf.parse::<Map>())
+            .collect::<Result<Vec<Map>, Box<dyn Error>>>()?;
+
+        Ok(Atlas { seeds, maps })
+    }
+}
+
+impl Atlas {
+    fn location_for_seed(&self, seed: u32) -> u32 {
+        let mut current = seed;
+        for map in self.maps.iter() {
+            current = map.translate(current);
+        }
+        current
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let atlas: Atlas = read_to_string("input")?.parse()?;
+
+    println!("minimum {}", atlas
+        .seeds
+        .iter()
+        .map(|&seed| atlas.location_for_seed(seed))
+        .min().unwrap());
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const TEST: &str = "\
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+";
+
+    #[test]
+    fn test_parse() {
+        let atlas: Atlas = TEST.parse().unwrap();
+
+        assert_eq!(atlas.location_for_seed(79), 82);
+        assert_eq!(atlas.location_for_seed(14), 43);
+        assert_eq!(atlas.location_for_seed(55), 86);
+        assert_eq!(atlas.location_for_seed(13), 35);
+    }
+}
