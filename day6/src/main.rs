33178ee207af7e8427895e269db28b98b69e1966
Neil Williams <neil@spladug.net>
2023-12-09 14:10:33 -0800

    Day 6, part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -5,59 +5,39 @@ use std::str::FromStr;
 use std::vec::Vec;
 
 struct Race {
-    time: u32,
-    distance: u32,
+    time: u64,
+    distance: u64,
 }
 
 impl Race {
-    fn ways_to_win(&self) -> impl Iterator<Item = u32> + '_ {
+    fn ways_to_win(&self) -> impl Iterator<Item = u64> + '_ {
         (0..self.time)
             .map(|hold_time| hold_time * (self.time - hold_time))
             .filter(|&distance_traveled| distance_traveled > self.distance)
     }
 }
 
-struct PieceOfPaper {
-    races: Vec<Race>,
-}
-
-impl FromStr for PieceOfPaper {
+impl FromStr for Race {
     type Err = Box<dyn Error>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (time_line, distance_line) = s.split_once('\n').ok_or("couldn't split into lines")?;
+        let parsed: Vec<u64> = s
+            .lines()
+            .map(|line| line.split_whitespace().skip(1).collect::<String>().parse())
+            .collect::<Result<Vec<u64>, ParseIntError>>()?;
 
-        let times = time_line
-            .split_whitespace()
-            .skip(1)
-            .map(|n| n.parse::<u32>())
-            .collect::<Result<Vec<u32>, ParseIntError>>()?;
-
-        let distances = distance_line
-            .split_whitespace()
-            .skip(1)
-            .map(|n| n.parse::<u32>())
-            .collect::<Result<Vec<u32>, ParseIntError>>()?;
-
-        let races = times
-            .iter()
-            .zip(distances.iter())
-            .map(|(&time, &distance)| Race { time, distance })
-            .collect();
-
-        Ok(Self { races })
+        if let [time, distance] = parsed[..] {
+            Ok(Self { time, distance })
+        } else {
+            Err("incorrect number of lines".into())
+        }
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let paper: PieceOfPaper = read_to_string("input")?.parse()?;
+    let race: Race = read_to_string("input")?.parse()?;
 
-    let total = paper
-        .races
-        .iter()
-        .map(|race| race.ways_to_win().count())
-        .reduce(|acc, count| acc * count)
-        .ok_or("no races!?")?;
+    let total = race.ways_to_win().count();
     println!("{total}");
 
     Ok(())
@@ -74,14 +54,10 @@ Distance:  9  40  200
 
     #[test]
     fn test_parsing() {
-        let paper: PieceOfPaper = TEST.parse().unwrap();
+        let race: Race = TEST.parse().unwrap();
 
-        assert_eq!(paper.races[0].time, 7);
-        assert_eq!(paper.races[0].distance, 9);
-        assert_eq!(paper.races[1].time, 15);
-        assert_eq!(paper.races[1].distance, 40);
-        assert_eq!(paper.races[2].time, 30);
-        assert_eq!(paper.races[2].distance, 200);
+        assert_eq!(race.time, 71530);
+        assert_eq!(race.distance, 940200);
     }
 
     #[test]
